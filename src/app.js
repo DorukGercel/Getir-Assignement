10b77621411068eede0c3275d0b5b285f522d4a1
DorukGercel <dorukgercel@hotmail.com>
2021-12-27 10:27:34 +0300

    src["feat"]: add cors options

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ const config = require("./configs");
 const load = require("./loaders");
 const {RecordRoutes, ErrorRoutes} = require("./routes")
 const {Logger} = require("./logger/Logger")
+const cors = require("cors")
 
 // Initialize process env configs
 config();
 // Load db and connect
 load();
+// Arrange CORS settings
+app.use(cors());
+app.options('*', cors());
 // Use json for API
 app.use(express.json());
 
